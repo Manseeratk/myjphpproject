4b58699c472f82876edaab0d9e0168ee7005c527
Manseeratk <126494428+Manseeratk@users.noreply.github.com>
2023-04-21 18:49:03 -0500

    Update header.php

--- header.php
+++ header.php
@@ -132,6 +132,7 @@
 	<!-- BEGIN: Content-->
     <div class="app-content content ">
         <div class="content-overlay"></div>
+	    
         <div class="header-navbar-shadow"></div>
         <div class="content-wrapper container-xxl p-0">
             <div class="content-header row">
