d7a3b84cf08ec5baa0e852ca5d5a621a815098a5
Manseeratk <126494428+Manseeratk@users.noreply.github.com>
2023-04-21 18:38:33 -0500

    Update deleteProduct.php

--- deleteProduct.php
+++ deleteProduct.php
@@ -6,7 +6,7 @@ include "php/auth.php";
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = [
         'status' => 0,
-        'id' => $_POST['product_id']
+        'id' => $_POST['product_id'] 
     ];
     
     $sql = "UPDATE products SET status=:status WHERE id=:id";
