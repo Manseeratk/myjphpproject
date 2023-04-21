d377b8e0353a55e9856e3c555cd01d1388565334
Manseeratk <126494428+Manseeratk@users.noreply.github.com>
2023-04-21 18:54:00 -0500

    Update login.php

--- login.php
+++ login.php
@@ -39,7 +39,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html class="loading" lang="en" data-textdirection="ltr">
 <!-- BEGIN: Head-->
 
-<head>
+
+	<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
